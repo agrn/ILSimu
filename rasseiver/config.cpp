d72833340ac22aba90678877b1f5a7b6b285aa15
Alban Gruin <alban@pa1ch.fr>
2019-04-10 11:57:46 +0200

    rasseiver: add functions to parse a config file
    This adds a parser for config files.  Their syntax is quite simple:
    
       device = airspy
       frequency = 111100000
       sample_rate = 2500000
       …
    
    For now, the parser is very simple.  Perhaps it will be improved in the future.
    
    This also adds two `==' operators for ConfigValue, and a default configuration.
    
    Signed-off-by: Alban Gruin <alban@pa1ch.fr>

--- rasseiver/config.cpp
+++ rasseiver/config.cpp
@@ -1,8 +1,8 @@
-#include <fstream>
-#include <map>
-#include <sstream>
-
 #include "config.h"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
 
 ConfigValue::ConfigValue(): value {""} {
 }
@@ -10,7 +10,7 @@ ConfigValue::ConfigValue(): value {""} {
 ConfigValue::ConfigValue(std::string const &value): value {value} {
 }
 
-ConfigValue::operator std::string() const {
+std::string ConfigValue::get_value() const {
 	return this->value;
 }
 
@@ -20,4 +20,74 @@ ConfigValue::operator double() const {
 
 ConfigValue::operator int() const {
 	return std::stoi(value);
+}
+
+ConfigValue::operator unsigned int() const {
+	return (unsigned int) std::stoi(value);
+}
+
+bool ConfigValue::operator==(const std::string &s) const {
+	return this->value == s;
+}
+
+bool ConfigValue::operator==(const ConfigValue &o) const {
+	return this->value == o.value;
+}
+
+static inline void ltrim(std::string &s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+				return !std::isspace(ch);
+			}));
+}
+
+static inline void rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+				return !std::isspace(ch);
+			}).base(), s.end());
+}
+
+static inline void trim(std::string &s) {
+	ltrim(s);
+	rtrim(s);
+}
+
+static void parse_line(std::string &line,
+		       std::map<std::string, ConfigValue> &values) {
+	std::string key, value;
+	bool has_value = false;
+
+	for (auto it = line.begin(); it != line.end(); ++it) {
+		if (*it == '#') {
+			break;
+		} else if (*it == '=') {
+			has_value = true;
+		} else if (*it == '\\') {
+			++it;
+		} else if (has_value) {
+			value.append(1, *it);
+		} else {
+			key.append(1, *it);
+		}
+	}
+
+	if (has_value) {
+		trim(key);
+		trim(value);
+
+		values[key] = ConfigValue {value};
+	}
+}
+
+void config_read_file(std::string file, ConfigMap &values) {
+	std::ifstream config {file};
+	std::string line;
+
+	if (!config.fail()) {
+		while (std::getline(config, line)) {
+			parse_line(line, values);
+		}
+	} else {
+		std::cerr << "Failed to open file \"" << file << "\""
+			  << std::endl;
+	}
 }
--- rasseiver/config.h
+++ rasseiver/config.h
@@ -1,6 +1,7 @@
 #ifndef __ILSIMU_RASSEIVER_CONFIG_H
 # define __ILSIMU_RASSEIVER_CONFIG_H
 
+# include <map>
 # include <string>
 
 class ConfigValue {
@@ -8,12 +9,28 @@ class ConfigValue {
   ConfigValue();
   ConfigValue(std::string const &value);
 
-  operator std::string() const;
+  std::string get_value() const;
+
   operator double() const;
   operator int() const;
+  operator unsigned int() const;
+
+  bool operator==(const std::string &s) const;
+  bool operator==(const ConfigValue &o) const;
 
  private:
   std::string value;
 };
+
+using ConfigMap = std::map<std::string, ConfigValue>;
+
+const ConfigMap config_default = {
+  {"device", ConfigValue {"airspy"}},
+  {"frequency", ConfigValue {"111100000"}},
+  {"sample_rate", ConfigValue {"2500000"}},
+  {"sample_type", ConfigValue {"int"}},
+};
+
+void config_read_file(std::string file, ConfigMap &values);
 
 #endif  /* __ILSIMU_RASSEIVER_CONFIG_H */
