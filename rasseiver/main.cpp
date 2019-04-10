185bd2038279cffedef23197257aa147478462a1
Alban Gruin <alban@pa1ch.fr>
2019-04-10 12:00:08 +0200

    rasseiver: read the configuration file in main.cpp
    This adds the ability for the user to pass a config file as an argument, and to
    use it to configure Rasseiver.
    
    If the user specifies "airspy" as the device, it will try to open an Airspy with
    the specified configuration.  Otherwise, it will fail as we do not support any
    other device yet.
    
    Signed-off-by: Alban Gruin <alban@pa1ch.fr>

--- rasseiver/main.cpp
+++ rasseiver/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
 
+#include "config.h"
 #include "device_airspy.h"
 
-int main() {
-	Airspy airspy;
+void run_airspy(ConfigMap const &config) {
+	Airspy airspy {config.at("frequency"),
+			config.at("sample_rate"),
+			AIRSPY_SAMPLE_INT16_IQ};
 
 	std::cout << "hello, world" << std::endl;
+}
+
+int main(int argc, char **argv) {
+	ConfigMap config {config_default};
+
+	if (argc > 2) {
+		std::cerr << "Usage: " << argv[0] << " [config file]"
+			  << std::endl;
+		return EXIT_FAILURE;
+	} else if (argc == 2) {
+		std::cout << "Using config file" << std::endl;
+		config_read_file(argv[1], config);
+	}
+
+	if (config["device"] == "airspy") {
+		run_airspy(config);
+	} else {
+		std::cerr << "Unknown device type \""
+			  << config["device"].get_value()
+			  << "\"" << std::endl;
+		return EXIT_FAILURE;
+	}
+
 	return EXIT_SUCCESS;
 }
