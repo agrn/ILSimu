c5b0851c72e5bc9f73b68120fbc6339d532cdaf1
Alban Gruin <alban@pa1ch.fr>
2019-04-15 15:10:49 +0200

    rasseiver: stop when sigint or sigterm is received
    Until now, there was no clean way to stop the program.  This adds a way to do so
    properly, by sending a sigint or a sigterm to the program.
    
    As the program is multi-threaded, we have to mask the two signals we want to
    handle with pthread_sigmask(3), and then wait for them with sigwait(3).
    
    Signed-off-by: Alban Gruin <alban@pa1ch.fr>

--- rasseiver/main.cpp
+++ rasseiver/main.cpp
@@ -1,23 +1,58 @@
 #include <iostream>
 
+// Signal handling
+#include <csignal>
+#include <pthread.h>
+
+#include "circular_buffer.hpp"
 #include "config.hpp"
 #include "device_airspy.hpp"
 
 /**
- * Run the program with an Airspy device.
+ * Run the program with an Airspy device.  Stops when a signal in the sigset is
+ * received.
+ *
+ * TODO: sigset should be part of another function, perhaps.
  *
  * @param config The configuration of the device
+ * @param set List of signals to wait for
  */
-void run_airspy(ConfigMap const &config) {
+void run_airspy(ConfigMap const &config, sigset_t &set) {
 	Airspy airspy {config.at("frequency"),
 			config.at("sample_rate"),
 			AIRSPY_SAMPLE_INT16_IQ};
+	int sig;
+
+	airspy.receive();
 
 	std::cout << "hello, world" << std::endl;
+	sigwait(&set, &sig);
+}
+
+/**
+ * Init a sigset_t and use it as a signal mask for every threads.
+ *
+ * The initialisation step consists of clearing the mask, and adding SIGINT and
+ * SIGTERM to the mask.
+ *
+ * @param set The signal set to init.
+ */
+int setup_sigmask(sigset_t &set) {
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	sigaddset(&set, SIGTERM);
+
+	if (pthread_sigmask(SIG_BLOCK, &set, nullptr)) {
+		perror("pthread_sigmask()");
+		return -1;
+	}
+
+	return 0;
 }
 
 int main(int argc, char **argv) {
 	ConfigMap config {config_default};
+	sigset_t set;
 
 	// Check program parameters
 	if (argc > 2) {
@@ -32,9 +67,14 @@ int main(int argc, char **argv) {
 		config_read_file(argv[1], config);
 	}
 
+	// Setup signals
+	if (setup_sigmask(set)) {
+		return EXIT_FAILURE;
+	}
+
 	// Check the device type, and call the appropriate function
 	if (config["device"] == "airspy") {
-		run_airspy(config);
+		run_airspy(config, set);
 	} else {
 		// Unknown device
 		std::cerr << "Unknown device type \""
