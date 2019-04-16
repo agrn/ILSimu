70e8a38547c65d3bcffcb43840b890611187bc1e
Alban Gruin <alban@pa1ch.fr>
2019-04-16 15:40:23 +0200

    rasseiver/filter: add an implementation of an FIR filter
    This adds an implementation of an FIR filter.  The function computes the
    convolution between the input and the filter, and inserts the result in a
    std::vector<>.
    
    This implementation also can perform a decimation on the input buffer by filtering
    only the values that will be copied to the output, and should be faster
    than filtering the entire input, and then decimating the result.
    
    Signed-off-by: Alban Gruin <alban@pa1ch.fr>

--- rasseiver/filter.hpp
+++ rasseiver/filter.hpp
@@ -4,6 +4,8 @@
 # include <string>
 # include <vector>
 
+# include "circular_buffer.hpp"
+
 using Filter = std::vector<double>;
 
 /**
@@ -13,5 +15,41 @@ using Filter = std::vector<double>;
  * @param values The vector where the read values are stored.
  */
 void filter_read_file(std::string file, Filter &filter);
+
+/**
+ * FIR implementation.  The input buffer is convoluted with the filter.
+ *
+ * This function does not permorm any kind of bound checking when accessing to
+ * values in the buffer.  Make sure it is big enough.
+ *
+ * @param buffer The buffer to filter.
+ * @param filter The values of the FIR.
+ * @param output The output.
+ * @param begin The index of the first element to filter.
+ * @param step The decimation factor.  Only one value out of `step` is
+ *   filtered.  The result is the same as if the filter was applied to the
+ *   whole input, and then decimated, but it's faster to do both at the
+ *   same time.
+ * @param channels Number of channels in the input.  Useful when the input
+ *   is an IQ-demodulated signal.
+ */
+template<typename T>
+size_t filter_buffer(CircularBuffer<T> const &buffer, Filter const &filter,
+		     std::vector<T> &output,
+		     size_t begin, int step, int channels) {
+	size_t i;
+
+	for (i = begin; i < buffer.size(); i += step * channels) {
+		T value {};
+
+		for (size_t j = 0; j < filter.size(); ++j) {
+			value += buffer[i - (j * channels)] * filter[j];
+		}
+
+		output.push_back(value);
+	}
+
+	return i;
+}
 
 #endif  /* __ILSIMU_RASSEIVER_FILTER_HPP */
