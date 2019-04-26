f57b66648c5ff39d9905e1c49508c010a25a0e3c
Alban Gruin <alban@pa1ch.fr>
2019-04-26 11:29:45 +0200

    ilserver: use asyncio
    Signed-off-by: Alban Gruin <alban@pa1ch.fr>

--- ilserver/__main__.py
+++ ilserver/__main__.py
@@ -1,49 +1,44 @@
 #!/usr/bin/env python3
 
 import array
-import socket
-import threading
+import asyncio
 
 
-class ClientThread(threading.Thread):
-    def __init__(self, ip, port, clientSocket):
-        threading.Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.clientSocket = clientSocket
+async def listener(reader, writer):
+    ip, port = writer.get_extra_info("peername")
+    print("Connection of {}:{}".format(ip, port))
 
-    def run(self):
-        print("Connection of {}:{}".format(self.ip, self.port))
+    with open("results.csv", "w") as csv:
+        while True:
+            data = await reader.read(2048)
+            if not data:
+                break
 
-        with open("results-{}:{}.csv".format(self.ip, self.port), "w") as csv:
-            while True:
-                data = self.clientSocket.recv(2048) # 65536 * 4 / 60 = 4369.066…
-                if not data:
-                    break
+            decoded = array.array("h", data)
+            i = 0
+            while i < len(decoded):
+                n = abs(complex(decoded[i], decoded[i + 1]))
+                csv.write("{},\n".format(int(n)))
+                i += 2
 
-                decoded = array.array('h', data)
-                i = 0
-                while i < len(decoded):
-                    n = abs(complex(decoded[i], decoded[i + 1]))
-                    csv.write("{},\n".format(int(n)))
-                    i += 2
-
-        print("Client {}:{} disconnected".format(self.ip, self.port))
+    print("{}:{} disconnected".format(ip, port))
+    writer.close()
 
 
 def main():
-    tcpSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcpSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    tcpSock.bind(("", 10000))
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(listener, "127.0.0.1", 10000, loop=loop)
+    server = loop.run_until_complete(coro)
 
     print("Listening…")
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
-    while True:
-        tcpSock.listen(10)
-        clientSocket, (ip, port) = tcpSock.accept()
-
-        newThread = ClientThread(ip, port, clientSocket)
-        newThread.start()
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
 
 
 if __name__ == "__main__":
