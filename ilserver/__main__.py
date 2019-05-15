ab583fd19a629ba97361a14d17e285cd037ea913
Alban Gruin <alban@pa1ch.fr>
2019-05-15 10:30:24 +0200

    ilserver: add a controller server
    This adds a controller server to control the phase shift to be applied
    to each channel.  Each processed packet is then sent to the controller.
    
    When the controller connects, the server sends the number of channels.
    
    Each packet is prepended by a boolean to indicate whether or not the
    phase shift was updated for that packet.
    
    For now, it is not safe to have multiple controllers, but this should be
    addressed in the future.
    
    Signed-off-by: Alban Gruin <alban@pa1ch.fr>

--- ilserver/__main__.py
+++ ilserver/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from contextlib import suppress
+from itertools import chain
 from concurrent.futures import ThreadPoolExecutor
 
 import array
@@ -26,7 +27,29 @@ channels_mutex = asyncio.Lock()
 
 executor = ThreadPoolExecutor(max_workers=1)
 
+controller_writer = None
 phase_shift = [0] * CHANNEL_AMOUNT
+old_phase_shift = [0] * CHANNEL_AMOUNT
+
+
+async def send_back_data(writer, packet, shift):
+    global old_phase_shift
+    assert len(packet) == PACKET_SIZE
+
+    if writer is None:
+        return
+
+    try:
+        to_send = chain.from_iterable((c.real, c.imag) for c in packet)
+        data = struct.pack("<?{}d".format(PACKET_SIZE * 2),
+                           old_phase_shift != shift,
+                           *to_send)
+        writer.write(data)
+        await writer.drain()
+        old_phase_shift = shift
+    except Exception as e:
+        print(e)
+        writer.close()
 
 
 def process_channel(loop, decoded, channel):
@@ -49,6 +72,9 @@ def process_channel(loop, decoded, channel):
 
                 res += c
                 del ch.buffer[:PACKET_SIZE]
+
+            asyncio.run_coroutine_threadsafe(send_back_data(controller_writer,
+                                                            res, shift), loop)
 
 
 async def channel_listener(reader, writer):
@@ -109,14 +135,41 @@ async def channel_listener(reader, writer):
             channels[channel_id] = None
 
 
+async def controller_listener(reader, writer):
+    global controller_writer, phase_shift
+    print("Connection of controller")
+
+    writer.write(struct.pack("<B", CHANNEL_AMOUNT))
+    await writer.drain()
+
+    controller_writer = writer
+
+    try:
+        while True:
+            raw_phase_shift = await reader.read(8 * CHANNEL_AMOUNT)
+            if not raw_phase_shift:
+                break
+
+            phase_shift = array.array("d", raw_phase_shift)
+
+    finally:
+        print("Controller disconnected")
+        writer.close()
+        controller_writer = None
+
+
 def main():
     # Open servers
     loop = asyncio.get_event_loop()
     servers = []
 
+    coro = asyncio.start_server(controller_listener, "127.0.0.1",
+                                BASE_CHANNEL - 1, loop=loop)
+    servers.append(loop.run_until_complete(coro))
+
     for i in range(CHANNEL_AMOUNT):
-        coro = asyncio.start_server(listener, "127.0.0.1", BASE_CHANNEL + i,
-                                    loop=loop)
+        coro = asyncio.start_server(channel_listener, "127.0.0.1",
+                                    BASE_CHANNEL + i, loop=loop)
         servers.append(loop.run_until_complete(coro))
 
     print("Listening…")
