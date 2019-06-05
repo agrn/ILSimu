dbc66b2b8ce4a9c960e1bf2c2f1d8f69c29b65d1
Alban Gruin <alban@pa1ch.fr>
2019-06-05 14:39:38 +0200

    rasseiver: add a dummy device
    This adds a class emulating a device generating IQ samples.  It may be
    configured to generate a determined or infinite amount of samples.
    
    Signed-off-by: Alban Gruin <alban@pa1ch.fr>

---
+++ rasseiver/src/device_dummy.cpp
@@ -0,0 +1,54 @@
+#include "device_dummy.hpp"
+
+#include <chrono>
+#include <numeric>
+
+DummyDevice::DummyDevice(int count): count {count} {
+}
+
+DummyDevice::~DummyDevice() {
+	stop();
+}
+
+void DummyDevice::receive(Process<int16_t> &process) {
+	running = true;
+	thd = std::thread {
+		[&]() {
+			std::array<int16_t,
+				   DummyDevice::device_bufsize * 2> data;
+			int count {0};
+
+			while (running &&
+			       (DummyDevice::count < 0 ||
+				count++ < DummyDevice::count)) {
+
+				std::iota(data.begin(), data.end(), 0);
+				process.apply(data.data(), data.size());
+
+				std::this_thread::sleep_for(
+					std::chrono::milliseconds {26});
+			}
+
+			running = false;
+		}
+	};
+}
+
+void DummyDevice::stop() {
+	if (running || thd.joinable()) {
+		running = false;
+		thd.join();
+	}
+}
+
+size_t DummyDevice::buffer_size() {
+	return DummyDevice::device_bufsize;
+}
+
+int DummyDevice::max_value() {
+	return 4096;
+}
+
+bool DummyDevice::is_streaming() {
+	return running;
+}
---
+++ rasseiver/src/device_dummy.hpp
@@ -0,0 +1,46 @@
+#ifndef __ILSIMU_RASSEIVER_DEVICE_DUMMY_HPP
+# define __ILSIMU_RASSEIVER_DEVICE_DUMMY_HPP
+
+# include "device.hpp"
+
+# include <atomic>
+# include <thread>
+
+/**
+ * A class to emulate a device generating IQ samples.
+ */
+class DummyDevice: public Device<int16_t> {
+public:
+	// No need for a default constructor.
+	DummyDevice() = delete;
+
+	/**
+	 * Creates a dummy device to create fake IQ samples.
+	 *
+	 * @param count The amount of samples to be generated before
+	 *   disconnecting.  If it is negative, samples are generated until
+	 *   it is stopped with `stop()'.
+	 */
+	DummyDevice(int count);
+
+	/**
+	 * Closes the device.
+	 */
+	~DummyDevice() override;
+
+	void receive(Process<int16_t> &process) override;
+	void stop() override;
+
+	size_t buffer_size() override;
+	int max_value() override;
+	bool is_streaming();
+
+private:
+	std::atomic<bool> running {false};
+	std::thread thd;
+	const int count;
+
+	static constexpr size_t device_bufsize {65536};
+};
+
+#endif  /* __ILSIMU_RASSEIVER_DEVICE_DUMMY_HPP */
